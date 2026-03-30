b7b8ef2920b8e5d0dae112dd6b56b0e4ded62375
joangq <jgquiroga@dc.uba.ar>
2026-03-30 17:07:43 -0300

    Inspect dataset

--- src/argendata_datasets/__init__.py
+++ src/argendata_datasets/__init__.py
@@ -26,7 +26,10 @@ def _default_get(id: str):
     datasets = client.datasets
     target = datasets.clean if int(c) > 0 else datasets.raw
 
-    return target.insepect(id)
+    response = target.inspect(id)
+    response.raise_for_status()
+
+    return response.content
     
 
 class Proxy:
