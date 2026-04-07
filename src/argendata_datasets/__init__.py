29f5305ebd608078e355cae3a97998b1da309b75
Joan G. Quiroga <jgquiroga@dc.uba.ar>
2026-04-07 12:54:21 -0300

    Update __init__.py

--- src/argendata_datasets/__init__.py
+++ src/argendata_datasets/__init__.py
@@ -28,7 +28,6 @@ def _default_get(id: str):
     from argendata_internal_client import Client, Settings
     import re
     client = Client.default()
-    client.login()
     pattern = re.compile('R([0-9]+)C([0-9]+)')
     matches = pattern.match(id)
 
